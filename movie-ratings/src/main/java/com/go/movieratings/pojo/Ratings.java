f25b7edd956ddbbe241ab8baa53e70d124d2d60a
Gaurav Ogale <gauravogale95@gmail.com>
2022-01-08 13:10:12 +0530

    Movie Rating Service Example

---
+++ movie-ratings/src/main/java/com/go/movieratings/pojo/Ratings.java
@@ -0,0 +1,34 @@
+package com.go.movieratings.pojo;
+
+public class Ratings {
+
+	private String movie;
+	private double ratings;
+	
+	public Ratings() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Ratings(String movie, double ratings) {
+		super();
+		this.movie = movie;
+		this.ratings = ratings;
+	}
+
+	public String getMovie() {
+		return movie;
+	}
+
+	public void setMovie(String movie) {
+		this.movie = movie;
+	}
+
+	public double getRatings() {
+		return ratings;
+	}
+
+	public void setRatings(double ratings) {
+		this.ratings = ratings;
+	}
+	
+}
---
+++ movie-ratings/src/test/java/com/go/movieratings/MovieRatingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.go.movieratings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRatingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
